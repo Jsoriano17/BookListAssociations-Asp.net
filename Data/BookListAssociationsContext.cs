0072474058f75b85e05d67cb7098732e72f04701
Jsoriano17 <52586265+Jsoriano17@users.noreply.github.com>
2020-08-20 23:26:12 -0600

    first commit

---
+++ Data/BookListAssociationsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookListAssociations.Models;
+
+namespace BookListAssociations.Data
+{
+    public class BookListAssociationsContext : DbContext
+    {
+        public BookListAssociationsContext (DbContextOptions<BookListAssociationsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookListAssociations.Models.Book> Book { get; set; }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListAssociations.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Author { get; set; }
+        public string Genere { get; set; }
+        public string Price { get; set; }
+    }
+}
